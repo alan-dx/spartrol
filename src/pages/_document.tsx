c909c741d51245db2729fe58ed1cd0c8eedc95c4
Alan Almeida <alanalmeida.emp@gmail.com>
2021-07-02 11:07:48 -0300

    Initial commit: install typescript, next-pwa

---
+++ src/pages/_document.tsx
@@ -0,0 +1,32 @@
+import Document, { Head, Main, NextScript, Html } from 'next/document'
+
+export default class MyApp extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta name='application-name' content="spartrol" />
+          <meta name='apple-mobile-web-app-capable' content='yes' />
+          <meta name='apple-mobile-web-app-status-bar-style' content='default' />
+          <meta name='apple-mobile-web-app-title' content="spartrol" />
+          <meta name='description' content="An application to manage your expenses" />
+          <meta name='format-detection' content='telephone=no' />
+          <meta name='mobile-web-app-capable' content='yes' />
+          <meta name='theme-color' content='#F5F5F5' />
+
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500;600&display=swap" rel="stylesheet" />
+
+          {/* <link rel='apple-touch-icon' sizes='180x180' href='/icons/apple-touch-icon.png' /> */}
+          <link rel='manifest' href='/manifest.json' />
+          <link rel='shortcut icon' href='/favicon.ico' />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+
+      </Html>
+    )
+  }
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+  return (
+    <h1>Spartrol</h1>
+  )
+}
