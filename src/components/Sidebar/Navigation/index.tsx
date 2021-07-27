2e927803e9df5dd0765952cad5237d17857b9b7f
Alan Almeida <alanalmeida.emp@gmail.com>
2021-07-27 00:19:07 -0300

    edit Navigation component to close sidebar when navigate to another page

--- src/components/Sidebar/Navigation/index.tsx
+++ src/components/Sidebar/Navigation/index.tsx
@@ -3,21 +3,41 @@ import { FiHome, FiPieChart, FiList, FiAward, FiUser, FiLogOut } from 'react-ico
 import styles from './styles.module.scss';
 import { useRouter } from 'next/dist/client/router';
 import { signOut } from 'next-auth/client';
+import { useContext } from 'react';
+import { SidebarContext } from '../../../contexts/SidebarContext';
 
 
 export function Navigation() {
 
+  const {isOpen, toogleSideBar} = useContext(SidebarContext)
   const router = useRouter()
 
   return (
     <ul className={styles.container}>
-      <SidebarItem onClick={() => router.push('home')} text="Home" icon={<FiHome size={30} color="#4F4F4F" />} />
-      <SidebarItem onClick={() => router.push('metrics')} text="Relatórios" icon={<FiPieChart size={30} color="#4F4F4F" />} />
-      <SidebarItem onClick={() => router.push('categories')} text="Categorias" icon={<FiList size={30} color="#4F4F4F" />} />
-      <SidebarItem onClick={() => router.push('goals')} text="Metas" icon={<FiAward size={30} color="#4F4F4F" />} />
+      <SidebarItem onClick={() => {
+        router.push('home')
+        toogleSideBar()
+      }} text="Home" icon={<FiHome size={30} color="#4F4F4F" />} />
+      <SidebarItem onClick={() => {
+        router.push('metrics')
+        toogleSideBar()
+      }} text="Relatórios" icon={<FiPieChart size={30} color="#4F4F4F" />} />
+      <SidebarItem onClick={() => {
+        router.push('categories')
+        toogleSideBar()
+      }} text="Categorias" icon={<FiList size={30} color="#4F4F4F" />} />
+      <SidebarItem onClick={() => {
+        router.push('goals')
+        toogleSideBar()
+      }} text="Metas" icon={<FiAward size={30} color="#4F4F4F" />} />
       <div className={styles.divider} />
-      <SidebarItem onClick={() => router.push('user')} text="Meu perfil" icon={<FiUser size={30} color="#4F4F4F" />} />
-      <SidebarItem onClick={() => signOut()}  text="Sair da conta" icon={<FiLogOut size={30} color="#4F4F4F" />} />
+      <SidebarItem onClick={() => {
+        router.push('user')
+        toogleSideBar()
+      }} text="Meu perfil" icon={<FiUser size={30} color="#4F4F4F" />} />
+      <SidebarItem onClick={() => {
+        signOut()
+      }}  text="Sair da conta" icon={<FiLogOut size={30} color="#4F4F4F" />} />
     </ul>
   )
 }
