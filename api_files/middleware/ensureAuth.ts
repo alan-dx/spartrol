1d9b0466d62518cc0f225c1687484b37e28acff0
Alan Almeida <alanalmeida.emp@gmail.com>
2022-01-17 11:42:27 -0300

    fix: bypass ensureAuth

--- api_files/middleware/ensureAuth.ts
+++ api_files/middleware/ensureAuth.ts
@@ -6,13 +6,15 @@ const secret = process.env.SECRET
 export const ensureAuth = (fn: NextApiHandler) => {
   return async (req: NextApiRequest, res: NextApiResponse) => {
 
-    const token = await getToken({ req, secret })
+    // const token = await getToken({ req, secret })
 
-    if (token) {
-      return await fn(req, res)
-    } else {
-      //Isn't necessary manually logout the user cause nextauth do it be default when the token is invalid
-      return res.status(401).json({message: 'Not authorized'}) 
-    }
+    // if (token) {
+    //   return await fn(req, res)
+    // } else {
+    //   //Isn't necessary manually logout the user cause nextauth do it be default when the token is invalid
+    //   return res.status(401).json({message: 'Not authorized'}) 
+    // }
+    //bypass
+    return await fn(req,res)
   }
 }
